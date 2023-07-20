ac3124049067576147c24e38d9feb8b0f68cdd6b
Al MustashR <121734476+almustashaar@users.noreply.github.com>
2023-07-20 10:25:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".text", {
+    strings:["Dev : Ahmed Makram"] ,
+    typeSpeed:200,
+    backSpeed:200,
+    backDelay:1000,
+    loop:true
+});
+
+
+var typed = new Typed(".text2", {
+    strings:["Web Devoleper and Graphic Designer"] ,
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:false
+});
+
+
